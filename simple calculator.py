a1a14eacb39aab97a03b3141d4abd8169439fffa
Tuiba <46740903+Tuiba@users.noreply.github.com>
2019-02-20 09:51:20 +0530

    Create simple calculator.py

---
+++ simple calculator.py
@@ -0,0 +1,12 @@
+x=int(input("enter first number= "))
+y=int(input("enter second number= "))
+sum=x+y
+diff=x-y
+pro=x*y
+div=x/y
+mod=x%y
+print("sum is- ",sum)
+print("difference is- ",diff)
+print("multiplication is- ",pro)
+print("division is-", div)
+print("modulus is- ",mod)
